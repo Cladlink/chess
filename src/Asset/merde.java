eec256ecb84779c6338f9a609a69ebfeecd2c853
Cladlink <michael.boutboul87@gmail.com>
2020-03-04 20:27:33 +0100

    merde + classe case

---
+++ src/Asset/merde.java
@@ -0,0 +1,4 @@
+package Asset;
+
+public class merde {
+}
--- src/Model/Case.java
+++ src/Model/Case.java
@@ -1,4 +1,15 @@
 package Model;
 
 public class Case {
+    private Piece pieceSurLaCase;
+    private int x;
+    private int y;
+    private boolean estBlanc;
+
+    public Case(Piece pieceSurLaCase, int x, int y) {
+        this.pieceSurLaCase = pieceSurLaCase;
+        this.x = x;
+        this.y = y;
+
+    }
 }
